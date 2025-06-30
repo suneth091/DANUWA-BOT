93981de5d405f60b5e068dfca566caaf6e453142
suneth091 <appheroku8@gmail.com>
2025-06-30 09:05:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,12 +19,12 @@ const defaultConfig = {
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
   WEATHER_API_KEY: "", // Add your weather API key here
-  BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
-  ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
+  BOT_OWNER: "94765844940", // Replace your bot owner number here with 94(country code)
+  ownerNumber: ["94765844940"], // Replace your bot owner number here (same as bot owner number)
   AUTO_STATUS_REACT: "true",
   AUTO_STATUS_REPLY: "true",
   AUTO_STATUS_SEEN: "true",
-  MODE: "public", // 'private', 'public'
+  MODE: "private", // 'private', 'public'
 };
 
 //*******************************************************************************************************************************************************
